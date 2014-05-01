df0eb278a1f13e4df68fd7c00f11e38760deac9b
Jesse Waites <jesse.waites@gmail.com>
2014-05-01 09:45:37 -0400

    added CSS card flip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429190148) do
+ActiveRecord::Schema.define(version: 20140429190018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
