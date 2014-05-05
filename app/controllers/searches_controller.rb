5ae785e06c1ee8980e355b40629542b0f73b65b5
Jesse Waites <jesse.waites@gmail.com>
2014-05-05 12:38:28 -0400

    completed searching multiple models with pg

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,8 @@
+class SearchesController < ApplicationController
+  def show
+    @query = params[:search][:query]
+    @results = PgSearch.multisearch(
+      @query)
+  end
+end
+
--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,4 +1,5 @@
 class Cohort < ActiveRecord::Base
+  include PgSearch
   has_many :users
   has_many :cities
 end
